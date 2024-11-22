2dbc71698155dde675b0837debb359e325f271eb
Brian <brianjackson1224@gmail.com>
2024-11-21 20:56:25 -0800

    Setting up variable height

--- src/components/YappoMeter.vue
+++ src/components/YappoMeter.vue
@@ -1,8 +1,30 @@
+<script setup lang="ts">
+    import {ref} from "vue";
+
+    const height = ref(0);
+
+    const setHeight = (h: number) =>{
+        height.value = h;
+    }
+</script>
+
+
 <template>
     <div class="meter-container">
-        <div class="meter-bar"></div>
+        <div class="meter-bar" style="height: barHeight"></div>
     </div>
 </template>
+
+
+<script lang="ts">
+    export default {
+        computed: {
+            barHeight() {
+                return height.value;
+            }
+        }
+    }
+</script>
 
 <style scoped>
     .meter-container {
@@ -10,7 +32,6 @@
         height: 80vh;
         width: 50vw;
         background-color: aquamarine;
-        border: 1px gray solid;
         box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.4);
     }
 
